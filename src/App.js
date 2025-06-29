1d719ba0de528fce869f544fcd14ed39e6462017
Piyush Shinde <workwithpiyush77@gmail.com>
2025-06-29 12:16:54 +0530

    added loader

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   
   const handleSearch = () => {
     if(!value.trim()) return;
+    setLoading(true);
     fetch(`https://api.weatherapi.com/v1/current.json?key=4cb09fbd67894756985155321252806&q=${value}`)
     .then((response) => response.json())
     .then((data) => {
@@ -25,6 +26,9 @@ function App() {
       console.log(error);
       alert("Failed to fetch weather data");
     })
+    .finally(() =>{
+      setLoading(false);
+    })
   }
   console.log(city);
   // console.log(city);
@@ -35,28 +39,33 @@ function App() {
       <input type='text' onChange={(e) => setValue(e.target.value)}></input>
       <button onClick={handleSearch}>Search</button>
 
-      {showData && (
-        
-        <div className='cards'>
-          <div className='card'>
-            <h4>Temperature</h4>
-            <p>{city.current.temp_c}⁰C</p>
+      {loading? (
+        <p>Loading. . .</p>
+      ) : (
+        showData && (
+          
+          <div className='cards'>
+            <div className='card'>
+              <h4>Temperature</h4>
+              <p>{city.current.temp_c}⁰C</p>
+            </div>
+            <div className='card'>
+              <h4>Humidity</h4>
+              <p>{city.current.humidity}%</p>
+            </div>
+            <div className='card'>
+              <h4>Condition</h4>
+              <p>{city.current.condition.text}</p>
+            </div>
+            <div className='card'>
+              <h4>Wind Speed</h4>
+              <p>{city.current.wind_kph}kph</p>
+            </div>
           </div>
-          <div className='card'>
-            <h4>Humidity</h4>
-            <p>{city.current.humidity}%</p>
-          </div>
-          <div className='card'>
-            <h4>Condition</h4>
-            <p>{city.current.condition.text}</p>
-          </div>
-          <div className='card'>
-            <h4>Wind Speed</h4>
-            <p>{city.current.wind_kph}kph</p>
-          </div>
-        </div>
 
+        )
       )}
+
 
     </div>
   );
